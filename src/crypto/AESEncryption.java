fc84ca40177622edfd75f5b7f3cb3ea0d87ce0ba
SAKSHAM KHANDELWAL <sakshamkhandelwal19207@gmail.com>
2026-01-08 12:21:02 +0530

    Update AESEncryption.java

--- src/crypto/AESEncryption.java
+++ src/crypto/AESEncryption.java
@@ -8,13 +8,13 @@ public class AESEncryption {
 
     private static final String KEY = "1234567890123456";
 
-    public static void encryptFile(Path input, Path output) throws Exception {
+    public static void encrypt(Path input, Path output) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(KEY.getBytes(), "AES"));
         Files.write(output, cipher.doFinal(Files.readAllBytes(input)));
     }
 
-    public static void decryptFile(Path input, Path output) throws Exception {
+    public static void decrypt(Path input, Path output) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(KEY.getBytes(), "AES"));
         Files.write(output, cipher.doFinal(Files.readAllBytes(input)));
